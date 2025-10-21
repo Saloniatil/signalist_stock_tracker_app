b71a03ba1461343ead51fbe1194fe51789526e92
Saloni Patil <salonipatil10122001@gmail.com>
2025-10-21 08:46:21 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
- import React from 'react'
- import { Button } from '@/components/ui/button'
- const Home = () => {
-   return (
-     <div className='flex jsutify-center items-center h-screen'>
-       <Button>Click Me</Button>
-     </div>
-   )
- }
- 
- export default Home
- 
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className ="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    
+    </div>
+  )
+}
+
+export default Home
+
